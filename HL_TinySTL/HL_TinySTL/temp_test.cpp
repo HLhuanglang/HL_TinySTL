3f53db68625d9159956474f54c065454c1770873
ECJTUhl <1282424466@qq.com>
2019-11-01 11:02:58 +0800

    添加内存处理工具函数，并测试。

--- HL_TinySTL/HL_TinySTL/temp_test.cpp
+++ HL_TinySTL/HL_TinySTL/temp_test.cpp
@@ -1,20 +1,44 @@
 #include<iostream>
 
 #include"allocator.h"
+#include"uninitialized.h"
+using std::ostream;
+using std::cout;
+using std::endl;
+
 class test {
+public:
+	test() : a(1), b('a'){}
+	test(int a) : a(a), b('A') {}
+	friend ostream &operator<<(ostream &out, test& item);
 private:
 	int a;
 	char b;
-	test* p;
 };
+ostream &operator<<(ostream &out, test& item) {
+	out << item.a << " " << item.b;
+	return out;
+}
 
 int main() {
-	for (int i = 1; i != 100000; ++i) {
-		auto p = allocator<test>::allocate();
-		allocator<test>::construct(p, test());
-		allocator<test>::destory(p);
-		allocator<test>::deallocate(p);
+
+	for (int i = 1; i <= 20; ++i) {
+		auto p = allocator<test>::allocate(5); //申请5个元素大小的空间
+		auto q = allocator<test>::allocate(5);
+		TinySTL::uninitialized_fill(p, p+5, test(i)); //申请了多大，就只能对这个范围进行填充，p- p+6还是填充5个
+		TinySTL::uninitialized_copy(p, p + 5, q);
+		for(int j=0; j<5; ++j)
+			cout << *(p+j) <<"  "<<*(q+j)<< endl;
+		cout << "------"<<endl;
 	}
+	cout << "****************************************" << endl;
+	int array[20];
+	TinySTL::uninitialized_fill_n(array, 20, 22); //对array填充20个22
+	for (auto n : array)
+	{
+		cout << n << endl;
+	}
+
 	system("pause");
 	return 0;
 }
---
+++ HL_TinySTL/HL_TinySTL/uninitialized.h
@@ -0,0 +1,93 @@
+/*********************************************************
+名称：uninitialized.h
+作用：提供内存处理工具函数
+**********************************************************/
+#ifndef UNINITIALIZED_H
+#define UNINITIALIZED_H
+
+#include"type_traits.h"
+#include"construct.h"
+#include"iterator.h"
+
+#include<cstring>
+namespace TinySTL {
+	/*****************************************************
+	名称：uninitialized_copy
+	作用：将[first,last) 复制到[result，result+(last-first) )
+	*****************************************************/
+	template<typename InputIterator, typename ForwardIterator>
+	ForwardIterator _uninitialized_copy_aux(InputIterator first,InputIterator last,
+		ForwardIterator result, __true_type){
+			//暂时用这个，后续使用copy函数
+		memcpy(result, first, (last - first) * sizeof(*first));
+		return result + (last - first);
+	}
+
+	template<typename InputIterator, typename ForwardIterator>
+	ForwardIterator _uninitialized_copy_aux(InputIterator first, InputIterator last,
+		ForwardIterator result, __false_type) {
+		ForwardIterator cur = result;
+		for (; first != last; ++first,++cur)
+			TinySTL::construct(&*cur, *first);
+		return result;
+	}
+
+	template<typename InputIterator, typename ForwardIterator>
+	ForwardIterator uninitialized_copy(InputIterator first, InputIterator last, ForwardIterator result) {
+		typedef typename __type_traits<iterator_traits<ForwardIterator>::value_type>::is_POD_type		IS_POD_TYPE;
+		return _uninitialized_copy_aux(first, last, result, IS_POD_TYPE());
+	}
+
+
+	/***********************************************************************
+	名称：uninitialized_fill
+	作用：对给定的范围[first，last)，按值为x进行对象构造
+	************************************************************************/
+	template<typename ForwardIterator,typename T>
+	void _uninitialized_fill_aux(ForwardIterator first, ForwardIterator last, const T& value, __true_type) {
+		//后续使用fill算法进行替换
+		for (; first != last; ++first)
+			*first = value;
+	}
+	template<typename ForwardIterator, typename T>
+	void _uninitialized_fill_aux(ForwardIterator first, ForwardIterator last, const T& value, __false_type) {
+		ForwardIterator cur = first;  //不能更改来源范围的迭代器
+		for (; cur != last; ++cur)
+			TinySTL::construct(&*cur, value);
+	}
+
+	template<typename ForwardIterator, typename T>
+	void uninitialized_fill(ForwardIterator first, ForwardIterator last, const T& value) {
+		typedef typename __type_traits<T>::is_POD_type		IS_POD_TYPE;
+			_uninitialized_fill_aux(first, last, value, IS_POD_TYPE());
+	}
+
+
+	/**********************************************************************
+	名称：uninitialized_fill_n
+	作用：对[first，first+n*sizeof(T)）范围内进行n个相同对象的构造
+	***********************************************************************/
+	template<typename ForwardIterator, typename Size,typename T>
+	ForwardIterator _uninitialized_fill_n_aux(ForwardIterator first, Size n, const T& value, __true_type) {
+		for (; n>0; --n,++first)
+			*first = value;
+		return first;
+ 	}
+
+	template<typename ForwardIterator , typename Size, typename T>
+	ForwardIterator _uninitialized_fill_n_aux(ForwardIterator first, Size n, const T& value, __false_type) {
+		ForwardIterator cur = first;
+		for (; n > 0; --n, ++cur)
+			TinySTL::construct(&*cur, value);
+		return first;
+	}
+
+	template<typename ForwardIterator, typename Size, typename T>
+	ForwardIterator uninitialized_fill_n(ForwardIterator first, Size n, const T& value) {
+		typedef typename __type_traits<iterator_traits<ForwardIterator>::value_type>::is_POD_type		IS_POD_TYPE;
+		_uninitialized_fill_n_aux(first, n, value, IS_POD_TYPE());
+		return first;
+	}
+
+} //namespace TinySTL
+#endif // !UNINITIALIZED_H
