631c62af070dc5229346fc66c07bb0b73f4ca820
ECJTUhl <1282424466@qq.com>
2019-12-04 15:46:22 +0800

    Revert "<test>(vector_test.h)将vector相关测试封装在一个vector_test()函数中"
    This reverts commit e5b3dc0a65c3fd7b061cd313dba6a7f1e54783ab.

--- HL_TinySTL/HL_TinySTL/temp_test.cpp
+++ HL_TinySTL/HL_TinySTL/temp_test.cpp
@@ -1,57 +1,123 @@
-#include"list.h"
+/*
+问题：
+
+*/
+#include"allocator.h"
+#include"uninitialized.h"
 #include"vector.h"
 
-#include<list>
-#include<vector>
 #include<iostream>
+#include<vector>
 #include<ctime>
+using std::ostream;
+using std::cout;
+using std::endl;
 
-bool val_to_delete(int x) { return x==4; }
+class test {
+public:
+	test() : a(1), b('a'){}
+	test(int a,char b) : a(a), b(b) {}
+	friend ostream &operator<<(ostream &out, test& item);
+private:
+	int a;
+	char b;
+};
+ostream &operator<<(ostream &out, test& item) {
+	out << item.a << " " << item.b;
+	return out;
+}
+
+template<typename T>
+void display(TinySTL::vector<T>& v) {
+	for (auto n : v) {
+		cout << n << endl;
+	}
+	cout << "--------------" << endl;
+}
+
+template<typename T>
+void big_data_test(TinySTL::vector<T>&v, int size) {
+	double start_time_tinystl = clock();
+	TinySTL::vector<T> v_tinystl;
+	for (int i = 0; i < 10000; i++) {
+		v_tinystl.push_back(T(i));  //T(i)这种写法只是为了区分当T是自定义类型时
+	}
+	double end_time_tinystl = clock();
+	cout << "TinySTL:push_back " << " " << end_time_tinystl - start_time_tinystl << endl;
+
+	double start_time_std = clock();
+	std::vector<T> v_std;
+	for (int i = 0; i < size; i++) {
+		v_std.push_back(T(i));
+	}
+	double end_time_std = clock();
+	cout << "std:push_back " << " " << end_time_std - start_time_std << endl;
+}
+
 
 int main() {
-	TinySTL::list<int> l1;
-	TinySTL::list<int>l2(1);
-	TinySTL::list<int>l3(10, 1);
-	TinySTL::list<int>l4(l3.begin(),l3.end());
-	TinySTL::list<int>l5(l3);
-	l5.push_back(11);
-	l5.push_front(9);
-	l5.pop_front();
-	l5.pop_back();
-	l1.push_back(1);
-	l1.push_back(1);
-	l1.push_back(2);
-	l1.push_back(3);
-	l1.push_back(3);
-	l1.push_back(4);
-	l1.push_back(5);
-	l1.push_back(6);
-	l1.push_back(7);
-	l1.push_back(8);
-	l1.remove_if(val_to_delete);
-	l1.unique();
-	l1.splice(l1.end(), l1, l1.begin());
-	for (auto n : l1) {
-		std::cout << n << std::endl;
-	}
+	//POD型
+	/***********************构造、copy等函数测试*******************************/
+	TinySTL::vector<int> v1;
+	TinySTL::vector<int> v2(10);
+	TinySTL::vector<int> v3(10, 100);
+	TinySTL::vector<int>v4(v3);
+	int a[] = { 1,2,3,4,5 };
+	TinySTL::vector<int> v5(a, a + 5);
+	//TinySTL::vector<int> v6 = v5;  v6直接指向了v5..
+	//TinySTL::vector<int> v7{ 1,2,3,4,5,6,7 }; 缺少这种构造函数
+	
+	cout << "***************************元素访问相关函数测试*****************************" << endl;
+	cout << "v5容器元素为：" << endl;
+	display(v5);
+	cout << "[ ]运算符访问" << " "<< v5[4] << endl;
+	cout << "front()函数：" << " "<< v5.front() << endl;
+	cout << "back()函数：" << " "<<  v5.back() << endl;
+
+	cout << "**************************容量相关函数测试*********************************" << endl;
+	cout << "v4容器元素为：" << endl;
+	display(v4);
+	cout << "v4.size()：" << " "<<  v4.size() << endl;
+	cout << "v4.capacity()：" << " "<< v4.capacity() << endl;
+
+	cout << "-------------reserve函数--------------------" << endl;
+	v4.reserve(9);
+	cout <<"size"<< v4.size() << endl;
+	cout << "capacity"<<v4.capacity() << endl;
+	display(v4);
+	cout << "---------" << endl;
+	v4.reserve(11);
+	cout << "size" << v4.size() << endl;
+	cout << "capacity" << v4.capacity() << endl;
+	display(v4);
+	cout << "----------------------------------------------" << endl;
 
 
-	//TinySTL::list<int> test1;
-	//std::list<int>test2;
+	cout << "-------------------resize函数----------------" << endl;
+	v4.resize(10);
+	cout << "resize(10)" << endl;
+	cout <<"size"<< v4.size() << endl;
+	cout <<"capacity"<<v4.capacity() << endl;
+	display(v4);
 
-	//double start_time = clock();
-	//for (int i = 0; i < 100000; i++) {
-	//	test1.push_front(i);
-	//}
-	//double end_time = clock();
-	//std::cout << end_time - start_time << std::endl;
+	v4.resize(11);
+	cout << "resize(11)" << endl;
+	cout << "size" << v4.size() << endl;
+	cout << "capacity" << v4.capacity() << endl;
+	display(v4);
+	cout << "------------------------------------------------" << endl;
 
-	//double start_time_std = clock();
-	//for (int i = 0; i < 100000; i++) {
-	//	test2.push_front(i);
-	//}
-	//double end_time_std = clock();
-	//std::cout << end_time_std - start_time_std << std::endl;
+
+	cout << "shrink_to_fit函数：" << endl;
+	v4.shrink_to_fit();
+	cout <<"size"<< v4.size() << endl;
+	cout <<"capacity"<< v4.capacity() << endl;
+	display(v4);
+	/******************************容器修改相关操作********************************/
+
+	big_data_test(v3, 10000);  //push_back测试
+
+
 	system("pause");
 	return 0;
 }
