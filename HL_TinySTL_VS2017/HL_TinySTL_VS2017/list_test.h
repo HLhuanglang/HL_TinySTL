1d95dcbd08f9463c6cff93713696d52d1e3a4416
ECJTUhl <1282424466@qq.com>
2019-12-04 16:37:15 +0800

    <test>(list.h)测试list容器相关成员函数

--- HL_TinySTL_VS2017/HL_TinySTL_VS2017/list_test.h
+++ HL_TinySTL_VS2017/HL_TinySTL_VS2017/list_test.h
@@ -1,3 +1,4 @@
+#pragma once
 #include"list.h"
 #include"vector.h"
 
@@ -6,13 +7,13 @@
 #include<iostream>
 #include<ctime>
 
-bool val_to_delete(int x) { return x==4; }
+bool val_to_delete(int x) { return x == 4; }
 
-int main() {
+void list_test() {
 	TinySTL::list<int> l1;
 	TinySTL::list<int>l2(1);
 	TinySTL::list<int>l3(10, 1);
-	TinySTL::list<int>l4(l3.begin(),l3.end());
+	TinySTL::list<int>l4(l3.begin(), l3.end());
 	TinySTL::list<int>l5(l3);
 	l5.push_back(11);
 	l5.push_front(9);
@@ -36,22 +37,20 @@ int main() {
 	}
 
 
-	//TinySTL::list<int> test1;
-	//std::list<int>test2;
+	TinySTL::list<int> test1;
+	std::list<int>test2;
 
-	//double start_time = clock();
-	//for (int i = 0; i < 100000; i++) {
-	//	test1.push_front(i);
-	//}
-	//double end_time = clock();
-	//std::cout << end_time - start_time << std::endl;
+	double start_time = clock();
+	for (int i = 0; i < 100000; i++) {
+		test1.push_front(i);
+	}
+	double end_time = clock();
+	std::cout << end_time - start_time << std::endl;
 
-	//double start_time_std = clock();
-	//for (int i = 0; i < 100000; i++) {
-	//	test2.push_front(i);
-	//}
-	//double end_time_std = clock();
-	//std::cout << end_time_std - start_time_std << std::endl;
-	system("pause");
-	return 0;
+	double start_time_std = clock();
+	for (int i = 0; i < 100000; i++) {
+		test2.push_front(i);
+	}
+	double end_time_std = clock();
+	std::cout << end_time_std - start_time_std << std::endl;
 }
